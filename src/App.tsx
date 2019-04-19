055e686890e2cb87eaaa72f32ca5d3981fc15174
mateja <mateja176@gmail.com>
2019-04-19 11:27:40 +0200

    Connects App to store

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, FC } from 'react';
+import Hello from './Hello';
+import { connect } from 'react-redux';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { State, selectName } from './store';
+import { Name, getName, setName, NameActionCreator } from './store/slices/name';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const setName$ = new Subject<Name>();
+
+export interface AppProps {
+  name: Name;
+  getName: NameActionCreator;
+  setName: NameActionCreator;
 }
 
-export default App;
+const App: FC<AppProps> = ({ name, setName, getName }) => {
+  const subscription = useEffect(() => {
+    getName();
+
+    setName$
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+      )
+      .subscribe(name => setName(name));
+
+    return () => subscription;
+  });
+
+  return (
+    <div>
+      <Hello name={name} />
+      <input onChange={({ target: { value } }) => setName$.next(value)} />
+    </div>
+  );
+};
+
+export default connect(
+  (state: State) => ({ name: selectName(state) }),
+  { getName, setName },
+)(App);
