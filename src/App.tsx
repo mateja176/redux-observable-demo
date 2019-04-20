098311e235b97cf0938b3054a953bc78c87b3845
Mateja Petrovic <mateja176@gmail.com>
2019-04-20 12:01:01 +0200

    Rewrites App using recompose

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import Hello from './Hello';
-import { connect } from 'react-redux';
-import { Subject, Subscription } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { State, selectName } from './store';
-import { Name, getName, setName, NameActionCreator } from './store/slices/name';
-import './App.css';
+import React, { FC } from "react";
+import { connect } from "react-redux";
+import { compose, lifecycle, withState } from "recompose";
+import { Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged } from "rxjs/operators";
+import "./App.css";
+import Hello from "./Hello";
+import { selectName, State } from "./store";
+import { getName, Name, NameActionCreator, setName } from "./store/slices/name";
 
 const setName$ = new Subject<Name>();
 
@@ -16,50 +17,42 @@ export interface AppProps {
 }
 
 export interface AppState {
-  subscription: Subscription | { unsubscribe: () => void };
+  subscription: { unsubscribe: () => void };
 }
 
-class App extends Component<AppProps, AppState> {
-  state = {
-    subscription: { unsubscribe: () => {} },
-  };
+const App: FC<AppProps> = ({ name }) => (
+  <div className="App">
+    <Hello name={name} />
+    <input onChange={({ target: { value } }) => setName$.next(value)} />
+  </div>
+);
 
-  componentDidMount() {
-    const {
-      props: { getName, setName },
-    } = this;
+export default compose(
+  connect(
+    (state: State) => ({ name: selectName(state) }),
+    { getName, setName },
+  ),
+  withState("subscription", "setSubscription", { unsubscribe: () => {} }),
 
-    getName();
+  lifecycle<AppProps, AppState>({
+    componentDidMount() {
+      const {
+        props: { getName, setName },
+      } = this;
 
-    const subscription = setName$
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged(),
-      )
-      .subscribe(name => setName(name));
+      getName();
 
-    this.setState({ subscription });
-  }
+      const subscription = setName$
+        .pipe(
+          debounceTime(500),
+          distinctUntilChanged(),
+        )
+        .subscribe(name => setName(name));
 
-  componentWillUnmount() {
-    this.state.subscription.unsubscribe();
-  }
-
-  render() {
-    const {
-      props: { name },
-    } = this;
-
-    return (
-      <div className="App">
-        <Hello name={name} />
-        <input onChange={({ target: { value } }) => setName$.next(value)} />
-      </div>
-    );
-  }
-}
-
-export default connect(
-  (state: State) => ({ name: selectName(state) }),
-  { getName, setName },
+      this.setState({ subscription });
+    },
+    componentWillUnmount() {
+      this.state.subscription.unsubscribe();
+    },
+  }),
 )(App);
